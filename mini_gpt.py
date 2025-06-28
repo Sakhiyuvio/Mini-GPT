055f4c5206e2961b23b0909ecf2e3e81b7ca2c1f
Sakhiyuvio <sakhiyfy@gmail.com>
2025-06-28 16:29:52 -0600

    data pre-processing done

--- mini_gpt.py
+++ mini_gpt.py
@@ -2,7 +2,7 @@ import logging
 import torch
 
 from corpus_builder import Dataset
-
+from enum import Enum
 """
 Making auto-regressive language model from scratch! 
 Play around with it! Currently trained with ~ 1 MB - 3MB of data, using sci-fi books for contextualization.
@@ -19,6 +19,13 @@ Play around with it! Currently trained with ~ 1 MB - 3MB of data, using sci-fi b
 # training proc
 
 # testing proc 
+
+class DataType(Enum):
+    TRAIN = "train"
+    TEST = "test"
+
+    def __str__(self):
+        return self.value
 
 class Tokenizer:
     def __init__(self, corpus_text: str):
@@ -41,8 +48,8 @@ class MiniGPT:
         logging.basicConfig(logging.INFO)
         self.logger.info("Enjoy this small language model, Mini-GPT!")
         self.output_path = output_path
-        self.window_size = 10  # context window size for the model, number of tokens to look back
-        self.batch_size = 4 # batch size for training in parallel
+        self.window_size = 32  # context window size for the model, number of tokens to look back
+        self.batch_size = 16 # batch size for training in parallel
 
     def pipeline(self):
         # read corpus
@@ -60,11 +67,13 @@ class MiniGPT:
         # split dataset: 0.9 train, 0.1 test
         all = len(corpus_encoded)
         train_size = int(0.9 * all)
-        train_corpus_data = corpus_encoded[:train_size]
-        test_corpus_data = corpus_encoded[train_size:]
+        self.train_corpus_data = corpus_encoded[:train_size]
+        self.test_corpus_data = corpus_encoded[train_size:]
 
-        # init model architecture: 10 context tokens 
-
+        # create batches for training
+        self.logger.info("Creating batches for training...")
+        x_train, y_train = self.create_batch(DataType.TRAIN)
+        x_val, y_val = self.create_batch(DataType.TEST)
 
     def read_corpus(self):
         # init dataset class for corpus init
@@ -75,3 +84,16 @@ class MiniGPT:
         with open(self.output_path, "r", encoding="utf-8") as f:
             corpus_text = f.read()
         return corpus_text
+
+    def create_batch(self, data_type: DataType):
+        data = self.train_corpus_data if data_type == DataType.TRAIN else self.test_corpus_data
+
+        torch.manual_seed(42) # Hitch-hiker's guide to the galaxy reference lol
+
+        # randomly pick indices to process data in mini batches, pick # batch size of window chunks
+        random_idx = torch.randint(len(data) - self.window_size, (self.batch_size,))
+        input_to_nn = torch.stack([data[i:i + self.window_size] for i in random_idx])
+        output_from_nn = torch.stack([data[i+1: i + 1 + self.window_size] for i in random_idx])
+        self.logger.info(f"Batch created for {data_type} data with shape: {input_to_nn.shape}, {output_from_nn.shape}")
+        return input_to_nn, output_from_nn
+
